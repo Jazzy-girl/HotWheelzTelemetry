fa30f685df1b32501e4aa49aab3071179158b6f9
Jazzy-girl <rsw5713@g.rit.edu>
2026-02-09 20:14:28 -0500

    made carSideGUI.py

---
+++ CarSideGUI/carSideGUI.py
@@ -0,0 +1,18 @@
+"""
+Authors: Ryanne Wilson
+
+Car-side GUI, using Raspberry Pi.
+Displays:
+    - Backup camera
+    - Speed
+    - Cockpit temperature
+    - Car battery
+    - Faults (if active)
+        - Low Cell Voltage
+        - Current Sensor
+        - Pack Voltage Sensor
+        - Thermistor
+
+Functionality:
+    - User can touch the screen to minize/fullscreen the backup camera
+"""
