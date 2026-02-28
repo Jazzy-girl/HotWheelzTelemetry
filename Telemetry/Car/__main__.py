99bb2eec823cd8ffc6d44f847fa1d2df530da079
Matthew Cornell <mcornell400708@gmail.com>
2026-02-28 14:05:37 -0500

    Integrate the GUI with the data collection

--- Telemetry/Car/__main__.py
+++ Telemetry/Car/__main__.py
@@ -1,11 +1,12 @@
-import board
-import busio
-import digitalio
 import time
 import base64
 
+import board
+import busio
+import digitalio
 import adafruid_rfm9x
 
+from Telemetry.Car.GUI import CarSideGUI
 from Telemetry.Car.Sensors.all import *
 from Telemetry.packet import RawPacket, ParsedPacket
 
@@ -21,11 +22,21 @@ lora_rst = digitalio.DigitalInOut(board.D5)
 lora_cs = digitalio.DigitalInOut(board.D13)
 lora = adafruit_rfm9x.RFM9x(spi0, lora_cs, lora_rst, 915.0)
 
-with open(time.strftime("logs/data_%Y%m%d_%H%M%S.csv", time.localtime()), "w+") as f: # a log name might be like "logs/data_20260204_210517.csv"
-    print(file=f, sep=",", *(ParsedPacket._fields + ("sent",))) # write all of the field names to the file, then "sent", all comma-separated
-    while True:
-        gps.update()
-        packet = RawPacket.new().apply(gps, bms, speed, thermistor)
-        data = packet.pack_bytes(True)
-        lora.send(data)
-        print(file=f, sep=",", *(packet.parse() + (base64.b64encode(data).decode('ascii'),))) # write all of the tuple fields to the file, then the packet itself, encoded as base64
+log_file = open(time.strftime("logs/data_%Y%m%d_%H%M%S.csv", time.localtime()), "w+")
+print(file=log_file, sep=",", *(ParsedPacket._fields + ("sent",))) # write all of the field names to the file, then "sent", all comma-separated
+
+gui: CarSideGUI = CarSideGUI()
+
+def update_data():
+    gps.update()
+    bms.update()
+    packet = RawPacket.new().apply(gps, bms, speed, thermistor)
+    data = packet.pack_bytes(True)
+    lora.send(data)
+    parsed = packet.parse()
+    print(file=log_file, sep=",", *(parsed + (base64.b64encode(data).decode('ascii'),))) # write all of the tuple fields to the file, then the packet itself, encoded as base64
+    gui.update_fields(parsed.motor_speed, parsed.bms_soc, parsed.therm_temp, parsed.bms_faults)
+    gui.root.after(100, update_data)
+
+update_data()
+gui.start()
