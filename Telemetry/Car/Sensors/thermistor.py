094b8728d4b7b2d46eed478b40e6fa8d08caa2a1
Matthew Cornell <mcornell400708@gmail.com>
2026-03-23 20:37:30 -0400

    Fix hardcoded pins

--- Telemetry/Car/Sensors/thermistor.py
+++ Telemetry/Car/Sensors/thermistor.py
@@ -2,16 +2,14 @@ import board
 import busio
 import digitalio
 import adafruit_mcp3xxx.mcp3008 as mcp
-from adafruit_mcp3xxx.analog_in import AnalogIn
 from Telemetry.packet import RawPacket
 from ..Sensors import SensorBase
 
 class CockpitThermistor(SensorBase):
     def __init__(self, spi: busio.SPI, cs: board.pin.Pin):
-        self.adc_cs = digitalio.DigitalInOut(board.D24)
+        self.adc_cs = digitalio.DigitalInOut(cs)
         self.adc = mcp.MCP3008(spi, self.adc_cs)
-        self.thermistor = AnalogIn(self.adc, mcp.P0)
     def reading(self) -> int:
-        return self.thermistor.value
+        return self.adc.read(0)
     def update_packet(self, packet: RawPacket) -> RawPacket:
         return packet._replace(temp=self.reading())
