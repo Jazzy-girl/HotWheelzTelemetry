52d942a46dcd453c3ef4afda2eabd9324eff64ca
Jazzy-girl <rsw5713@g.rit.edu>
2026-04-01 21:06:46 -0400

    added more testing data

--- Testing/CAN/mockBPS/mockBPS.ino
+++ Testing/CAN/mockBPS/mockBPS.ino
@@ -3,7 +3,7 @@
 #define ADDRESS 0x52
 
 // data to send over I2C
-byte data[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24 };
+byte data[] = {81, 0, 0, 0, 30, 0, 16, 39, 26, 39, 38, 2, 5, 0, 1, 0, 111, 11, 210, 21, 99, 9, 120, 4, 7};
 
 void setup() {
   Serial.begin(9600);
@@ -16,9 +16,9 @@ void setup() {
 void loop() {}
 void requestEvent() {
   Serial.println("Request!");
-  Wire.write(data, 24);
+  Wire.write(data, 25);
 }
-void receiveEvent() {
+void receiveEvent(int) {
   Serial.print("Receive: ");
   while (Wire.available()) {
     Serial.write(Wire.read());
