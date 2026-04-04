85eaf80543b8980f27372632447427bc3d3c3fc7
Jazzy-girl <rsw5713@g.rit.edu>
2026-04-04 13:03:57 -0400

    adding to Abstract Graph

--- Telemetry/Pit/GraphData.py
+++ Telemetry/Pit/GraphData.py
@@ -1,1 +1,17 @@
-from abc import ABC, abstractmethod
+from packet import ParsedPacket
+from collections import deque
+class GraphInputInterface():
+    def __init__(self, parsedPackets: deque[ParsedPacket], maxLength: int) -> None:
+        self.parsedPackets = parsedPackets
+        self.maxLength = maxLength
+
+    def addElement(self, el: ParsedPacket):
+        """
+        adds an element to the deque. Removes the leftmost element if necessary.
+        """
+        if(len(self.parsedPackets) == self.maxLength):
+            self.parsedPackets.popleft()
+        self.parsedPackets.append(el)
+    
+    def getDeque(self):
+        return self.parsedPackets
