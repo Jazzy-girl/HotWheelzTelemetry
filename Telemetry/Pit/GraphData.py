c68a9348551e2306a748ab0ba7c38078ab8a19d9
Jazzy-girl <rsw5713@g.rit.edu>
2026-04-11 11:44:25 -0400

    made GraphData interface and instantiations

--- Telemetry/Pit/GraphData.py
+++ Telemetry/Pit/GraphData.py
@@ -1,9 +1,12 @@
 from packet import ParsedPacket
 from collections import deque
-class GraphInputInterface():
+class GraphDataInterface():
     def __init__(self, parsedPackets: deque[ParsedPacket], maxLength: int) -> None:
         self.parsedPackets = parsedPackets
         self.maxLength = maxLength
+        self.data = deque()
+        for el in self.parsedPackets:
+            self.addData(el)
 
     def addElement(self, el: ParsedPacket):
         """
@@ -11,7 +14,40 @@ class GraphInputInterface():
         """
         if(len(self.parsedPackets) == self.maxLength):
             self.parsedPackets.popleft()
+            self.data.popleft()
         self.parsedPackets.append(el)
+        self.addData(el)
     
-    def getDeque(self):
-        return self.parsedPackets
+    def addData(self, el: ParsedPacket):
+        pass
+    
+    def getData(self):
+        return self.data
+
+class GraphDataCockpit(GraphDataInterface):
+    def __init__(self, parsedPackets: deque[ParsedPacket], maxLength: int) -> None:
+        super().__init__(parsedPackets, maxLength)
+    
+    def addData(self, el: ParsedPacket):
+        self.data.append(el.therm_temp)
+
+class GraphDataPOV(GraphDataInterface):
+    def __init__(self, parsedPackets: deque[ParsedPacket], maxLength: int) -> None:
+        super().__init__(parsedPackets, maxLength)
+    
+    def addData(self, el: ParsedPacket):
+        self.data.append(el.bms_open_voltage)
+
+class GraphDataCurrent(GraphDataInterface):
+    def __init__(self, parsedPackets: deque[ParsedPacket], maxLength: int) -> None:
+        super().__init__(parsedPackets, maxLength)
+    
+    def addData(self, el: ParsedPacket):
+        self.data.append(el.bms_current)
+
+class GraphDataHighest(GraphDataInterface):
+    def __init__(self, parsedPackets: deque[ParsedPacket], maxLength: int) -> None:
+        super().__init__(parsedPackets, maxLength)
+    
+    def addData(self, el: ParsedPacket):
+        self.data.append(el.bms_high_temp)
