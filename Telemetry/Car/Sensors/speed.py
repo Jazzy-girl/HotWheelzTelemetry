58fa01f077e4d044652fa8b306721bdb69a3c537
Matthew Cornell <mcornell400708@gmail.com>
2026-04-29 19:35:28 -0400

    Move the updated test implementation to the real car code

--- Telemetry/Car/Sensors/speed.py
+++ Telemetry/Car/Sensors/speed.py
@@ -20,14 +20,24 @@ class SpeedWorker(threading.Thread, SensorBase):
             self.start()
     def run(self):
         while True:
+            now = time.monotonic()
+            did_pulse = True
             while not self.motor.value:
                 time.sleep(0.001)
-            while self.motor.value:
-                time.sleep(0.001)
+                if time.monotonic() - now < 2:
+                    did_pulse = False
+                    break
+            if did_pulse:
+                while self.motor.value:
+                    time.sleep(0.001)
+                    if time.monotonic() - now < 2:
+                        did_pulse = False
+                        break
             now = time.monotonic()
-            while self.queue and now - self.queue[0] > 1:
+            while len(self.queue) and now - self.queue[0] > 1:
                 self.queue.popleft()
-            self.queue.append(now)
+            if did_pulse:
+                self.queue.append(now)
     def pulses(self) -> int:
         return len(self.queue)
     def update_packet(self, packet: RawPacket) -> RawPacket:
