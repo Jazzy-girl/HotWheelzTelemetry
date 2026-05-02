17ef8b4d07efd0abf572628aa5014ea419c88f75
Matthew Cornell <mcornell400708@gmail.com>
2026-05-02 15:01:43 -0400

    Integrate the GPS into the main code

--- LVBox/LVBox.ino
+++ LVBox/LVBox.ino
@@ -1,6 +1,7 @@
 #include "packet.h"
 #include "motor.h"
 #include "radio.h"
+#include "gps.h"
 #include <Wire.h>
 
 void setup() {
@@ -9,14 +10,19 @@ void setup() {
     Wire.begin();
     motor_controller_init();
     radio_init();
+    gps_init();
     packet.H = 'H';
     packet.W = 'W';
 }
 
 void loop() {
     motor_controller_poll();
+    gps_poll();
     packet.timestamp = millis();
     packet.motor_speed = motor_controller_pulses();
+    packet.longitude = gps_longitude;
+    packet.latitude = gps_latitude;
+    packet.gps_speed = gps_speed;
     swap_packet_bytes(&packet);
     write_checksum(&packet);
     radio_send();
