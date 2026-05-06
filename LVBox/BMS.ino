4246474965171a06ff2aa0cf9b2352ae81301ecb
Matthew Cornell <mcornell400708@gmail.com>
2026-05-06 19:41:19 -0400

    Configure I2C pins to be on A2 and A3 instead

--- LVBox/BMS.ino
+++ LVBox/BMS.ino
@@ -4,9 +4,13 @@
 
 #define FEATHER_ADDRESS 0x52
 #define I2C_FRAME_LEN 25
+#define BMS_SDA A2
+#define BMS_SCL A3
 
 /// Initialize the I2C connection
 void bms_init() {
+    Wire.setSDA(BMS_SDA);
+    Wire.setSCL(BMS_SCL);
     Wire.begin();
 }
 /// Read the I2C data into the global packet
