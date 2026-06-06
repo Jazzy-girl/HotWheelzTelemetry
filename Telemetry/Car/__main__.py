b143af415a652f8252153cf0add206e86dc235b4
Matthew Cornell <mcornell400708@gmail.com>
2026-06-06 13:14:25 -0400

    Remove now-unnecessary device initialization from the car main file

--- Telemetry/Car/__main__.py
+++ Telemetry/Car/__main__.py
@@ -1,22 +1,9 @@
 import time
 import base64
 
-import board
-import busio
-import digitalio
-import adafruit_rfm9x
-
 from Telemetry.Car.GUI import CarSideGUI
-from Telemetry.Car.Sensors.all import *
-from Telemetry.packet import RawPacket, ParsedPacket
+from Telemetry.packet import ParsedPacket
 import Telemetry.serial_recv as serial
-
-
-spi0 = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-
-lora_rst = digitalio.DigitalInOut(board.D5)
-lora_cs = digitalio.DigitalInOut(board.D13)
-lora = adafruit_rfm9x.RFM9x(spi0, lora_cs, lora_rst, 915.0)
 
 log_file = open(time.strftime("logs/data_%Y%m%d_%H%M%S.csv", time.localtime()), "w+")
 print(file=log_file, sep=",", *(ParsedPacket._fields + ("sent",))) # write all of the field names to the file, then "sent", all comma-separated
@@ -31,7 +18,6 @@ def update_data():
         packet = message.packet
         parsed = packet.parse()
         data = packet.pack_bytes(True)
-        lora.send(data)
         print(file=log_file, sep=",", *(parsed + (base64.b64encode(data).decode('ascii'),))) # write all of the tuple fields to the file, then the packet itself, encoded as base64
         gui.update_fields(parsed.motor_speed, parsed.bms_soc, parsed.therm_temp, parsed.bms_faults)
     gui.root.after(100, update_data)
