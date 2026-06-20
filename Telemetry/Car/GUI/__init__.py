e0a777ab31647ea8802d486883c08bc674308a34
Jazzy-girl <rsw5713@g.rit.edu>
2026-06-20 12:05:14 -0400

    fix3

--- Telemetry/Car/GUI/__init__.py
+++ Telemetry/Car/GUI/__init__.py
@@ -1,8 +1,8 @@
-import os
+# import os
 
-if os.environ.get('DISPLAY','') == '':
-    print('no display found. Using :0.0')
-    os.environ.__setitem__('DISPLAY', ':0.0')
+# if os.environ.get('DISPLAY','') == '':
+#     print('no display found. Using :0.0')
+#     os.environ.__setitem__('DISPLAY', ':0.0')
 
 """
 Authors: Ryanne Wilson
