156b5d190c4a9b80f4f3f2c9ae68c78d9ff2c5aa
Jazzy-girl <rsw5713@g.rit.edu>
2026-06-20 11:56:34 -0400

    added fix2

--- Telemetry/Car/GUI/__init__.py
+++ Telemetry/Car/GUI/__init__.py
@@ -1,5 +1,8 @@
-import matplotlib
-matplotlib.use('Agg')
+import os
+
+if os.environ.get('DISPLAY','') == '':
+    print('no display found. Using :0.0')
+    os.environ.__setitem__('DISPLAY', ':0.0')
 
 """
 Authors: Ryanne Wilson
