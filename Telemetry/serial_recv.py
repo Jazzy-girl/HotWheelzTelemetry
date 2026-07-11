e70df983a44f15cec66fc3c8798e204d77599056
Jazzy-girl <rsw5713@g.rit.edu>
2026-07-11 16:50:55 -0400

    maybe fixed freezing by changing serial_recv

--- Telemetry/serial_recv.py
+++ Telemetry/serial_recv.py
@@ -1,3 +1,4 @@
+import time
 import serial
 import os
 import io
@@ -12,13 +13,20 @@ class BackendInterface:
             if os.path.isfile(interface):
                 self.interface = open(interface)
             else:
-                ser = serial.Serial(interface, baudrate)
+                self.ser = serial.Serial(interface, baudrate, timeout=1)
+                # time.sleep(2)
                 # ser.open()
-                self.interface = io.TextIOWrapper(io.BufferedReader(ser), newline='\n')
+                # self.interface = io.TextIOWrapper(io.BufferedReader(ser), newline='\n')
+                print("connected!")
         else:
             self.interface = interface
     def read(self) -> 'BackendMessage':
-        return BackendMessage.parse(self.interface.readline())
+        # return BackendMessage.parse(self.interface.readline().strip())
+        raw = self.ser.readline().decode("ascii",errors="replace")
+        raw = raw[:-2]
+        # print(raw)
+        # print(raw.strip())
+        return BackendMessage.parse(raw)
     def __iter__(self) -> Iterator['BackendMessage']:
         return map(BackendMessage.parse, self.interface)
 
